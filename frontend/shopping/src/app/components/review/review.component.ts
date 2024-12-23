3f5ba13fb57137afedf4c80a8fa8f407f169b00a
nnicha123 <nicha_ngamtweerat@hotmail.com>
2024-12-23 11:07:48 +0700

    Fixed add a review section

--- frontend/shopping/src/app/components/review/review.component.ts
+++ frontend/shopping/src/app/components/review/review.component.ts
@@ -12,9 +12,27 @@ import { ReviewCustomerDetails } from '../../models/review-customer-details';
 export class ReviewComponent implements OnInit {
   @Input() productId: number = 0;
   reviews$: Observable<ReviewCustomerDetails[]> = of([]);
+
+  @Input() rating: number = 0;
+  @Input() fontSize: number = 20;
+  starsArray = Array(5).fill(false);
+
   constructor(private reviewService: ReviewService) {}
 
   ngOnInit(): void {
     this.reviews$ = this.reviewService.getReviewsByProduct(this.productId);
+    this.starsArray[0] = true;
+  }
+
+  changeRating(index: number) {
+    // case incrementing stars
+    for (let i = 0; i <= index; i++) {
+      this.starsArray[i] = true;
+    }
+    // decrementing
+    for (let i = 4; i > index; i--) {
+      this.starsArray[i] = false;
+    }
+    this.rating = index;
   }
 }
